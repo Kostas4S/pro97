789bf2c7d064ff08f9d5e0b2e83447e62c9bdbd5
Kostas4S <81780796+Kostas4S@users.noreply.github.com>
2022-01-26 19:04:39 -0500

    Update guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -1,12 +1,13 @@
 import random
 number = random.randint(1, 9)
 chanceCount = 0
-while chances < 5:
+while chanceCount < 5:
     introString = int(input("enter the number between 1 and 9: "))
     if (introString > number):
         print("Your guess is too large")
     elif (introString == number):
         print("Congratulations! You guessed it correctly")
+        break:
     else :
         print("YOU LOST!!")
     chanceCount = chanceCount + 1
